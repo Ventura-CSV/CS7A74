ce44190b26188986300ee9a9f6dc1a2bb06db100
Kyuwoong Lee <kwooler@gmail.com>
2023-02-23 00:36:01 -0800

    Initial commit

---
+++ main_test.py
@@ -0,0 +1,83 @@
+import main
+import io
+import sys
+import re
+import math
+
+
+def test_main_1():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    datastr = '25\n'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    # regex_string = r'[\w,\W]*1'
+    # regex_string += r'[\w,\W]*3'
+    # regex_string += r'[\w,\W]*5'
+    # regex_string += r'[\w,\W]*'
+    # print(regex_string)
+    # res = re.search(regex_string, main.evenlist)
+    # assert res != None
+    # print(res.group())
+    assert main.main.numbers[2] == 25
+
+
+def test_main_2():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    datastr = '0\n'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    # regex_string = r'[\w,\W]*1'
+    # regex_string += r'[\w,\W]*3'
+    # regex_string += r'[\w,\W]*5'
+    # regex_string += r'[\w,\W]*'
+    # print(regex_string)
+    # res = re.search(regex_string, main.evenlist)
+    # assert res != None
+    # print(res.group())
+    assert main.main.numbers[0] == 0
+
+
+def test_main_3():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    datastr = '100\n'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    # regex_string = r'[\w,\W]*1'
+    # regex_string += r'[\w,\W]*3'
+    # regex_string += r'[\w,\W]*5'
+    # regex_string += r'[\w,\W]*'
+    # print(regex_string)
+    # res = re.search(regex_string, main.evenlist)
+    # assert res != None
+    # print(res.group())
+    assert main.main.numbers[5] == 100
+
+
+def test_main_4():
+    result = False
+    with open('main.py') as f:
+        for line in f:
+            if 'sort' in line:
+                result = True
+    assert result == False
